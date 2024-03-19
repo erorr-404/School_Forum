cd32f24b8462e6820670012f34c2b0abd5cdf1c4
Erorr <maxymholub@gmail.com>
2024-03-19 12:54:52 +0200

    Accounts system

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('', views.profile_view, name='profile'),
-    path('register/', views.register_view, name='login'),
+    path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('edit_profile/', views.edit_profile_view, name='edit_profile'),
+    path('ajax-logout/', views.ajax_logout, name='ajax_logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.http import JsonResponse, HttpResponse
+from .forms import RegistrationForm
 
+
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 
 # Create your views here.
 def register_view(request):
-    return HttpResponse('Register')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("posts:categories")
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form':form})
 
 
 def login_view(request):
-    return HttpResponse('Login')
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if "next" in request.POST:
+                return redirect(request.POST.get("next"))
+            else:
+                return redirect("posts:categories")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {'form':form})
 
 
 def edit_profile_view(request):
@@ -17,3 +42,10 @@ def edit_profile_view(request):
 
 def profile_view(request):
     return HttpResponse('Your profile')
+
+
+def ajax_logout(request):
+    if is_ajax(request):
+        logout(request)
+        response = {'first-text': 'Logged out'}
+        return JsonResponse(response)
