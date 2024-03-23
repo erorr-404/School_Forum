817e9156901e8fdd9b6fcadbb3f46fa67a470023
Erorr <maxymholub@gmail.com>
2024-03-23 21:40:43 +0200

    Comments, titles, profiles
    - Added page titles
    - Added styles for comments
    - Part of profile page
    - Maybe few changes I forgot

--- posts/views.py
+++ posts/views.py
@@ -30,10 +30,25 @@ def post_list_view(request, category):
 
 def post_view(request, category, post):
     post = Post.objects.get(slug=post)
-    return render(request, 'post_detail.html', {'post':post, 
-                                                'likes':len(PostLike.objects.filter(post=post)),
-                                                'dislikes':len(PostDisLike.objects.filter(post=post)),
-                                                'comments':Comment.objects.filter(post=post)[::-1]
+    comments = Comment.objects.filter(post=post)[::-1]
+    comments_users_profiles = []
+
+    for comment in comments:
+        comment_user_profile = Profile.objects.get(user=comment.user)
+        comments_users_profiles.append(comment_user_profile)
+    
+    if len(comments) == 0:
+        no_comments = True
+    else:
+        no_comments = False
+    
+    return render(request, 'post_detail.html', {'post': post,
+                                                'author_profile': Profile.objects.get(user=post.author),
+                                                'likes': len(PostLike.objects.filter(post=post)),
+                                                'dislikes': len(PostDisLike.objects.filter(post=post)),
+                                                'comments': comments,
+                                                'zipped_comments': zip(comments, comments_users_profiles),
+                                                'no_comments': no_comments,
                                                 })
 
 
@@ -46,7 +61,7 @@ def create_post(request, category):
             instance.author = request.user
             instance.category = Category.objects.get(slug=category)
             instance.save()
-            return redirect('posts:categories')
+            return redirect(f'/{instance.category.slug}')
     else:
         form = forms.CreatePost()
     return render(request, 'create_post.html', {'form':form, 'category':category})
@@ -64,12 +79,17 @@ def like_post(request, category, post):
             like_status = True
             if user_this_post_dislikes > 0:
                 PostDisLike.objects.filter(post=liked_post, user=request.user).delete()
+                delete_dislike = True
+            else:
+                delete_dislike = False
         else:
             like_status = False
-    
-        response = {'likes':len(PostLike.objects.filter(post=liked_post)), 
-                    'dislikes':len(PostDisLike.objects.filter(post=liked_post)), 
-                    'like-status':like_status}
+            delete_dislike = False
+
+        response = {'likes': len(PostLike.objects.filter(post=liked_post)), 
+                    'dislikes': len(PostDisLike.objects.filter(post=liked_post)), 
+                    'like_status': like_status,
+                    'delete_dislike': delete_dislike}
         return JsonResponse(response)
 
 
@@ -85,12 +105,17 @@ def dislike_post(request, category, post):
             dislike_status = True
             if user_this_post_likes > 0:
                 PostLike.objects.filter(post=disliked_post, user=request.user).delete()
+                delete_like = True
+            else:
+                delete_like = False
         else:
             dislike_status = False
+            delete_like = False
     
-        response = {'likes':len(PostLike.objects.filter(post=disliked_post)), 
-                    'dislikes':len(PostDisLike.objects.filter(post=disliked_post)), 
-                    'dislike-status':dislike_status}
+        response = {'likes': len(PostLike.objects.filter(post=disliked_post)), 
+                    'dislikes': len(PostDisLike.objects.filter(post=disliked_post)), 
+                    'dislike_status': dislike_status,
+                    'delete_like': delete_like}
         return JsonResponse(response)
 
 
@@ -98,13 +123,16 @@ def dislike_post(request, category, post):
 def comment_post(request, category, post):
     if is_ajax(request):
         comment_text = request.POST.get('text')
-        comment_user = request.user
-        comment_date = timezone.now()
-        comment_post = Post.objects.get(slug=post)
+        if comment_text != '' and ' ':
+            comment_user = request.user
+            comment_profile_image = Profile.objects.get(user=request.user).image.url
+            comment_date = timezone.now()
+            comment_post = Post.objects.get(slug=post)
 
-        comment = Comment(user=comment_user, post=comment_post, text=comment_text)
-        comment.save()
+            comment = Comment(user=comment_user, post=comment_post, text=comment_text)
+            comment.save()
 
-        return JsonResponse({'text': comment_text, 
-                             'user_name':comment_user.username, 
-                             'date':comment_date})
+            return JsonResponse({'text': comment_text, 
+                                 'user_name': comment_user.username, 
+                                 'profile_image_url': comment_profile_image, 
+                                 'comment_date': comment_date})
